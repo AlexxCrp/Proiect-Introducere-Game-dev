8cb866f1dcd2f171ff6c20de75da448cd2a56201
Carpineanu_Alexandru <79210245+AlexxCrp@users.noreply.github.com>
2022-11-09 00:29:46 +0200

    Added First enemy prototype

---
+++ ProiectGaming/Assets/Scripts/BaseEnemyBulletController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyBulletController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float damage = 100;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag != "Enemy")
+        {
+            PlayerManager character = collider.GetComponent<PlayerManager>();
+            if (character != null)
+            {
+                character.TakeDamage(damage);
+            }
+            if (collider.gameObject.tag != "Enemy")
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ ProiectGaming/Assets/Scripts/BaseEnemyController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyController : MonoBehaviour
+{
+    public float HP = 1000;
+    public SpriteRenderer sprite;
+    public float range;
+    private Transform target;
+    public Transform firePoint;
+    public GameObject bullet;
+    public float fireRate;
+    // public Animator animator;
+
+    bool canShoot = true;
+    bool isDetected = false;
+
+    Vector2 direction;
+
+    public GameObject gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        Vector2 targetPosition = target.position;
+
+        direction = targetPosition - (Vector2)transform.position;
+
+        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, direction, range);
+
+        if (rayInfo)
+        {
+            if (!isDetected)
+            {
+                isDetected = true;
+                //animator.SetBool("isAgroed", true);
+            }
+            else
+            {
+                // animator.SetBool("isAgroed", false);
+                isDetected = false;
+            }
+        }
+
+        if (isDetected)
+        {
+            //gun.transform.up = direction * -1;
+            if (canShoot)
+            {
+                StartCoroutine(Shoot());
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+        yield return new WaitForSecondsRealtime(fireRate);
+        canShoot = true;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        HP -= damage;
+        StartCoroutine(FlashRed());
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator FlashRed()
+    {
+        sprite.color = Color.red;
+        yield return new WaitForSeconds(0.3f);
+        sprite.color = Color.white;
+
+    }
+}
